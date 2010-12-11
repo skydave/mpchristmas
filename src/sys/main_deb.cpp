c2f13c89f36466c790ef436789b746064dbc4bbb
David Koerner <dk402538@googlemail.com>
2010-12-11 00:23:49 +0000

    test2

---
+++ src/sys/main_deb.cpp
@@ -0,0 +1,35 @@
+//--------------------------------------------------------------------------//
+// iq . 2003 . code for the Paradise 64 kb intro by RGBA                    //
+//--------------------------------------------------------------------------//
+
+#define XRES        800
+#define YRES        600
+
+
+
+
+//----------------------------------------------------------------------------
+
+//void entrypoint( void )
+int main( void )
+{
+
+    return( 0 );
+}
+
+
+//#include <stdlib.h>
+//extern "C" {
+//int __cxa_pure_virtual()
+//{
+//	return 0;
+//}
+// commented out when using default libs
+///*
+//void _start( void )
+//{
+//	main();
+//	exit(0);
+//}
+//*/
+//}
